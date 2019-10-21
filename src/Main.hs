8ebfae650aa0a4ad93c52edb2d0c6a27b1a9bb3e
Evan Relf <evan@evanrelf.com>
2019-10-20 23:40:46 -0700

    Add command-line options parser

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,30 @@
 module Main (main) where
 
+import Options.Applicative
+
+data Options = Options
+  { input :: FilePath
+  , output :: FilePath
+  } deriving (Show)
+
+options :: Parser Options
+options = Options
+  <$> strOption (mconcat
+        [ long "input"
+        , short 'i'
+        , metavar "PATH"
+        , help "Input path"
+        ])
+  <*> strOption (mconcat
+        [ long "output"
+        , short 'o'
+        , metavar "PATH"
+        , help "Output path"
+        ])
+
+moire :: Options -> IO ()
+moire = print
+
 main :: IO ()
-main = putTextLn "Hello world"
+main = execParser opts >>= moire
+  where opts = info (helper <*> options) (header "moire" <> fullDesc)
